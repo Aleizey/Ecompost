8c499ddcb22f916d6eda8c0283738a009dbd0ce1
Alexloven <reyvaldiviaalejandro@gmail.com>
2024-11-23 19:56:50 +0100

    Creacion de las migraciones , orion, breeze

---
+++ app/Http/Controllers/Api/v1/CentroController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+// use App\Http\Controllers\Controller;
+use App\Models\Centro;
+use Orion\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+
+class CentroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    protected $model = Centro::class; 
+
+}
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Orion\Facades\Orion;
+use App\Http\Controllers\Api\v1\CentroController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::group(['as' => 'api.'], function() {
+    Orion::resource('centro', CentroController::class);
+});
+
