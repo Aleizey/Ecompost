a840fdafb1097039af5c00d6f69f2bbf355905e1
Alexloven <reyvaldiviaalejandro@gmail.com>
2024-12-07 16:00:07 +0100

    cambiando la compostera para que funcione al 100

--- app/Http/Controllers/Api/v1/BoloCiclosController.php
+++ app/Http/Controllers/Api/v1/BoloCiclosController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api\v1;
 use App\Models\Bolo;
 use Illuminate\Http\Request;
 use Orion\Concerns\DisableAuthorization;
+use Orion\Concerns\DisablePagination;
 use Orion\Http\Controllers\RelationController;
 
 class BoloCiclosController extends RelationController
@@ -14,6 +15,7 @@ class BoloCiclosController extends RelationController
      * Display a listing of the resource.
      */
     // use DisableAuthorization;
+    use DisablePagination;
     protected $model = Bolo::class;
 
     protected $relation = 'ciclos';
--- app/Http/Controllers/Api/v1/RegistroAntesController.php
+++ app/Http/Controllers/Api/v1/RegistroAntesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\v1;
 
 // use App\Http\Controllers\Controller;
+use Orion\Concerns\DisablePagination;
 use Orion\Http\Controllers\Controller;
 use App\Models\RegistroAntes;
 use Illuminate\Http\Request;
@@ -12,5 +13,6 @@ class RegistroAntesController extends Controller
     /**
      * Display a listing of the resource.
      */
+    use DisablePagination;
     protected $model = RegistroAntes::class; 
 }
